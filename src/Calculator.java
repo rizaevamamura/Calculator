dfbd9edb7a4968a53518a30edd588251123f5c26
rizaevamamura <rizaevamamura@gmail.com>
2019-01-19 11:57:14 -0800

    simple calculator method

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println(add(5, 6));
+    }
+
+    public static double add(double num1, double num2){
+        return num1 + num2;
+    }
+}
